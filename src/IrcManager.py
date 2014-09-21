810cdb65ea89468a65cc5677f64d07506a8f2eb9
Yousef Sultan <yousef.su.2000@gmail.com>
2014-09-21 17:11:21 +0300

    Fixed bad indentation

---
+++ src/IrcManager.py
@@ -0,0 +1,16 @@
+class ircManager:
+"""
+IRC proprietary handlers 
+"""
+    def register(s, nick, login, mode, realname, channel):
+        """
+        Register on network
+        """
+        s.send("PONG ".encode("utf-8") + s.recv(1024)[5:] + "\r\n".encode("utf-8"))
+        s.send(("NICK " + nick + "\r\n").encode("utf-8"))
+        s.send(("USER " + login + " " + mode + " * : " + realname + "\r\n").encode("utf-8"))
+        s.send("PONG ".encode("utf-8") + s.recv(1024)[5:] + "\r\n".encode("utf-8"))
+
+        #inputThread should be added
+
+        #A handler should exist here! [important]
