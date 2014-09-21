e5b24927f91c1cdc408eafbd5dacfaa05ed0a2f7
Tony Olagbaiye <frony0@gmail.com>
2014-09-21 18:20:26 +0100

    Essentially, you're using classes wrong.
    You’ve been using classes statically. That’s really bad…
    
    I’ve moved (almost) everything you need for now into the main class,
    PyBot. You should do stuff in there, using self, until it gets too
    large, then maybe move some things into another class called
    IrcConnection or something, in irc.py.
    
    I also removed your “external” class, I’m not sure why that existed.
    The last 6 lines of pybot.py show how a python main file should end.
    Run this using $ python3 pybot.py

--- irc.py
+++ irc.py
@@ -1,36 +1,15 @@
 
-class IrcManager:
+def parse(line) :
     """
-    IRC proprietary handlers 
+    Parse an irc line
     """
-    def register(s, nick, login, mode, realname, channel):
-        """
-        Register on network
-        """
-        s.send("PONG ".encode("utf-8") + s.recv(1024)[5:] + "\r\n".encode("utf-8"))
-        s.send(("NICK " + nick + "\r\n").encode("utf-8"))
-        s.send(("USER " + login + " " + mode + " * : " + realname + "\r\n").encode("utf-8"))
-        s.send("PONG ".encode("utf-8") + s.recv(1024)[5:] + "\r\n".encode("utf-8"))
-
-        #inputThread should be added
-
-        #A handler should exist here! [important]
-
-class IrcParser:
-    """
-    Offers Parsing tools
-    """
-    def parse(line) :
-        """
-        Parse an irc line
-        """
-        result = list()
-        splitter = line.split(" ")
-        result.append(line.split("!")[0][1:].lower()) #Name
-        result.append(splitter[0].split("!")[1].lower()) #Hostname
-        result.append(splitter[1].lower()) #Action
-        result.append(splitter[2].lower()) #Channel
-        result.append(splitter[3][1:].lower()) #Command
-        result.append(line.split(result[4]) + " "[1].lower()) #Arguments
-        return result
+    result = list()
+    splitter = line.split(" ")
+    result.append(line.split("!")[0][1:].lower()) #Name
+    result.append(splitter[0].split("!")[1].lower()) #Hostname
+    result.append(splitter[1].lower()) #Action
+    result.append(splitter[2].lower()) #Channel
+    result.append(splitter[3][1:].lower()) #Command
+    result.append(line.split(result[4]) + " "[1].lower()) #Arguments
+    return result
 
--- pybot.py
+++ pybot.py
@@ -1,65 +1,75 @@
 import socket
 
-from irc import IrcManager
+from irc import parse
+# I left parse in it's own file, only to show you how it's done.
+# When you've understood, move it into this class too, like register
 
-class main:
+class PyBot:
     """
     Main Bot Class
     """
+    def __init__(self):
+        """
+        Initialize the bot
+        """
+        self.server = "irc.subluminal.net"
+        self.port = 6667
+        self.nick = "pyBot"
+        self.login = "pyBot"
+        self.realname = "pyBot"
+        self.mode = "0"
+        self.channel = "#" + input("Channel: #")
+
     def connect(self):
         """
         Initiate Connection and respond to commands
         """
-        self.server = ""
-        self.port = 6667
-        self.nick = ""
-        self.login = ""
-        self.realname = ""
-        self.mode = ""
-        self.line = ""
-        self.channel = ""
-        
-        self.name = ""
-        self.hostname  = ""
-        self.chan = ""
-        self.action = ""
-        self.command = ""
-        self.args = ""
+        name = ""
+        hostname  = ""
+        chan = ""
+        action = ""
+        command = ""
+        args = ""
 
-        s = socket.socket()
+        s = socket.socket() # rename this to something better, and store it in self. preferably during __init__.
         try:
-            s.connect((server, port))
+            s.connect((self.server, self.port))
         except ConnectionRefusedError:
             print("Destination Net Unreachable")
 
-
-        register(s, nick, login, mode, realname, channel)
+        self.register(s, self.nick, self.login, self.mode, self.realname, self.channel)
 
         while True:
             line = s.recv(1024).decode("utf-8")
             if line:
                 #line should be parsed here
                 #Below code should be moved to ircManager.py
-                print (str(line))
+                print(str(line))
                 if "001" in line:
-                    s.send(("MODE " + mode + " +B\r\n").encode("utf-8"))
-                    s.send(("JOIN " + channel+ "\r\n").encode("utf-8"))
+                    s.send(("MODE " + self.mode + " +B\r\n").encode("utf-8"))
+                    s.send(("JOIN " + self.channel+ "\r\n").encode("utf-8"))
                 if "PING" in line[:4]:
                     s.send(("PONG " + line[5:] + "\r\n").encode("utf-8"))
 
-class PyBot:
-    """
-    External Bot class
-    """
-    def __init__(self):
+    def register(self, s, nick, login, mode, realname, channel): # Unused arguments!
         """
-        Initialize variables and Establish a connection
+        Register on network
         """
-        Main.server = "irc.subluminal.net"
-        Main.port = 6667
-        Main.nick = "pyBot"
-        Main.login = "pyBot"
-        Main.realname = "pyBot"
-        Main.mode = "0"
-        Main.channel = "#" + input("Channel: #")
-        Main.connect()
+	#You also have no reason to be using arguments for this now, since it's in the same class and can use self.
+        s.send("PONG ".encode("utf-8") + s.recv(1024)[5:] + "\r\n".encode("utf-8"))
+        s.send(("NICK " + nick + "\r\n").encode("utf-8"))
+        s.send(("USER " + login + " " + mode + " * : " + realname + "\r\n").encode("utf-8"))
+        s.send("PONG ".encode("utf-8") + s.recv(1024)[5:] + "\r\n".encode("utf-8"))
+
+        #inputThread should be added
+
+        #A handler should exist here! [important]
+        #Create and instantize a Handler class, and do something like handler.handle(line) here.
+
+if __name__ == "__main__":
+    """
+    Create the bot and run it
+    """
+    bot = PyBot()
+    bot.connect()
+
